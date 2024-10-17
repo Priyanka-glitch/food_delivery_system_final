d1e514ecc8dc88afaa42d70e3480c9cc4092e3bb
Priyanka-glitch <59969481+Priyanka-glitch@users.noreply.github.com>
2024-10-17 13:05:52 +0530

    Update RestaurantRepository.java

--- food_delivery_system_final/src/main/java/com/food_delivery_system/food_delivery_system/repo/RestaurantRepository.java
+++ food_delivery_system_final/src/main/java/com/food_delivery_system/food_delivery_system/repo/RestaurantRepository.java
@@ -1,7 +1,9 @@
 package com.food_delivery_system.food_delivery_system.repo;
 
+import com.food_delivery_system.food_delivery_system.model.MenuItem;
 import com.food_delivery_system.food_delivery_system.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,6 @@ import java.util.List;
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findAll();
-
+    @Query("SELECT r.menuItems FROM Restaurant r WHERE r.id = :restaurantId")
+    List<MenuItem> getMenuItems(Long restaurantId);
 }
