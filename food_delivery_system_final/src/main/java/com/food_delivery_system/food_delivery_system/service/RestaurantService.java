4405af00773f09f9f098f138ae08c0f89071ebcb
Priyanka-glitch <59969481+Priyanka-glitch@users.noreply.github.com>
2024-10-17 13:14:37 +0530

    Update RestaurantService.java

--- food_delivery_system_final/src/main/java/com/food_delivery_system/food_delivery_system/service/RestaurantService.java
+++ food_delivery_system_final/src/main/java/com/food_delivery_system/food_delivery_system/service/RestaurantService.java
@@ -1,20 +1,26 @@
 package com.food_delivery_system.food_delivery_system.service;
 
+import com.food_delivery_system.food_delivery_system.model.MenuItem;
 import com.food_delivery_system.food_delivery_system.model.Restaurant;
 import com.food_delivery_system.food_delivery_system.repo.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class RestaurantService {
 
     @Autowired
     private RestaurantRepository restaurantRepository;
 
     public Restaurant registerRestaurant(Restaurant restaurant) {
+        for (MenuItem menuItem : restaurant.getMenuItems()) {
+            menuItem.setRestaurant(restaurant);
+        }
         return restaurantRepository.save(restaurant);
     }
 
@@ -25,7 +31,11 @@ public class RestaurantService {
             restaurant.setName(restaurantDetails.getName());
             restaurant.setCapacity(restaurantDetails.getCapacity());
             restaurant.setRating(restaurantDetails.getRating());
-            restaurant.setMenuItems(restaurantDetails.getMenuItems());
+            //restaurant.getMenuItems().clear();
+            for (MenuItem newMenuItem : restaurantDetails.getMenuItems()) {
+                newMenuItem.setRestaurant(restaurant);
+                restaurant.getMenuItems().add(newMenuItem);
+            }
             return restaurantRepository.save(restaurant);
         } else {
             throw new RuntimeException("Restaurant not found with id: " + id);
